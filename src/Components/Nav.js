cfc0730bafaa399e621808807b1917d7dbee37bb
Julian Roost <j.roost@student.fontys.nl>
2019-11-01 13:06:48 +0100

    update logo picture

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Nav.css";
 
 function Nav() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="navbar-brand">
-        <img width="30" height="30" class="d-inline-block align-top"></img>
+        <img src="pineapple.jpg" width="30" height="30" class="d-inline-block align-top"></img>
         Ananas
       </div>
       <button
